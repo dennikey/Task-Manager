20dcad1770b7fc0e241ec65c6fc77331d7a938c6
dennikey <dennisbae7@gmail.com>
2020-01-26 15:51:52 -0500

    Create todosData.js

---
+++ todosData.js
@@ -0,0 +1,21 @@
+// Type in your tasks here in array format! Make sure to not miss or misspell any of the properties such as id, text, or completed!
+
+const todosData = [
+    {
+        id: 1,
+        text: "Take out the trash",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Grocery shopping",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Clean gecko tank",
+        completed: false
+    }
+]
+
+export default todosData
